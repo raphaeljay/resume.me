7af2d0e3721f2b81d9eea5f50e878a9ce4f05cb3
Raphael Jamaica <46942852+raphaeljay@users.noreply.github.com>
2025-09-30 20:06:30 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? '/resume.me/' : '/',
   server: {
     host: "::",
     port: 8080,
